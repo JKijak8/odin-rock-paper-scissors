f371cb314de668616f3496b9781feac586b4d530
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2024-04-09 04:10:05 +0200

    Add function for getting player's choice.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-  let choice = getRandom(1, 3);
+  const choice = getRandom(1, 3);
 
   switch (choice) {
     case 1:
@@ -9,6 +9,37 @@ function getComputerChoice() {
     case 3:
       return "Scissors";
   }
+}
+
+function getPlayerChoice() {
+  while (true) {
+    let choice = prompt("Enter your choice: ");
+    choice = choice.toLowerCase();
+    choice = capitalizeFirst(choice);
+
+    if (choice === "Rock" || choice === "Paper" || choice === "Scissors") {
+      return choice;
+    } else {
+      console.log("Wrong option. Please try again.");
+    }
+  }
+}
+
+function playRound() {
+  const playerSelection = getPlayerChoice();
+  const computerSelection = getComputerChoice();
+
+  if (
+    (computerSelection === "Rock" && playerSelection === "Scissors") ||
+    (computerSelection === "Paper" && playerSelection === "Rock") ||
+    (computerSelection === "Scissors" && playerSelection === "Paper")
+  ) {
+    console.log(`You lose! ${computerSelection} beats ${playerSelection}`);
+  }
+}
+
+function capitalizeFirst(string) {
+  return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
 function getRandom(min, max) {
