57e3bc3f1a6557b18d9c82ea343ac7a773edc9a9
JKijak8 <123284755+JKijak8@users.noreply.github.com>
2025-10-30 15:16:13 +0100

    feat: add function for playing round of rps.

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice() {
   const choices = ["rock", "paper", "scissors"];
 
@@ -7,3 +10,36 @@ function getComputerChoice() {
 function getHumanChoice() {
   return prompt("Choose 'rock', 'paper' or 'scissors'").toLowerCase();
 }
+
+function playRound(humanChoice, computerChoice) {
+  if (humanChoice === computerChoice) {
+    console.log("Tie! Nobody wins.");
+    playRound(getHumanChoice(), getComputerChoice());
+  } else if (
+    (humanChoice === "rock" && computerChoice === "scissors") ||
+    (humanChoice === "paper" && computerChoice === "rock") ||
+    (humanChoice === "scissors" && computerChoice === "paper")
+  ) {
+    console.log(
+      `You win! ${capitalize(humanChoice)} beats ${capitalize(computerChoice)}.`
+    );
+    humanScore++;
+  } else {
+    console.log(
+      `You lose! ${capitalize(computerChoice)} beats ${capitalize(
+        humanChoice
+      )}.`
+    );
+    computerScore++;
+  }
+}
+
+function capitalize(word) {
+  if (typeof word === "string" && word != false) {
+    return word.replace(word[0], word[0].toUpperCase());
+  } else return word;
+}
+
+const humanChoice = getHumanChoice();
+const computerChoice = getComputerChoice();
+playRound(humanChoice, computerChoice);
